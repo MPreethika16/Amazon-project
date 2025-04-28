ec893cb10cead889523e33d27c8d097f97c009ae
MPreethika <manepallypreethika@gmail.com>
2025-04-28 12:52:20 +0530

    added cart feature

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,35 +1,6 @@
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127
-    },
-    priceCents: 2095
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56
-    },
-    priceCents: 799
-  }
-];
-let productsHTML='';
+let productsHTML = "";
 products.forEach((product) => {
-  productsHTML+=`
+  productsHTML += `
         <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -74,12 +45,34 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+           data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>`;
-  
 });
-console.log(productsHTML);
+//console.log(productsHTML);
 
-document.querySelector('.js-products-grid').innerHTML=productsHTML;
+document.querySelector(".js-products-grid").innerHTML = productsHTML;
+document.querySelectorAll(".js-add-to-cart").forEach((button)=>{
+    button.addEventListener('click',()=>{ // corrected the typo here
+        const productId=button.dataset.productId;
+        let matchingItem;
+        cart.forEach((item)=>{
+            if(productId===item.productId){
+                matchingItem=item;
+            }
+        });
+        if(matchingItem){
+            matchingItem.quantity+=1;
+        }
+        else{
+            cart.push({
+                productId:productId,
+                quantity:1
+            });
+        }
+       
+        console.log(cart);
+    });
+});
